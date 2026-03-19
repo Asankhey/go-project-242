30f0a745ed813fa5097b6a8d97ada0fb23d15dc3
asankheya <lilitasa@proton.me>
2026-03-20 00:11:12 +0700

    move path_size.go into cmd folder

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
@@ -13,6 +14,16 @@ func main() {
 		Name:  "hexlet-path-size",
 		Usage: "print size of a file or directory",
 		Action: func(ctx context.Context, c *cli.Command) error {
+			if len(os.Args) < 2 {
+				fmt.Println("Please provide a path")
+				return nil
+			}
+			path := os.Args[1]
+			size, err := GetPathSize(path, false, false, false)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("%s\t%s\n", size, path)
 			return nil
 		},
 	}
